76097d8b891c0cd7165817769205e3feb3825e52
maks <nickitkacozloff@yandex.ru>
2023-08-30 18:39:23 +0300

    modified Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,58 +7,60 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class Differ {
-        public static String differ(String text1, String text2) throws Exception {
-            return getDiff(text1, text2);
+    public static String differ(String text1, String text2) throws Exception {
+        return generate(text1, text2);
+    }
+
+    public static String parse(String readFilePath) throws Exception {
+
+        Path path = Paths.get(readFilePath);
+        if (!Files.exists(path)) {
+            throw new Exception();
         }
 
-        public static String parse(String readFilePath) throws Exception {
+        return Files.readString(path);
+    }
 
-            Path path = Paths.get(readFilePath);
-            if (!Files.exists(path)) {
-                throw new Exception();
-            }
+    public static String generate(String text1, String text2) throws JsonProcessingException {
 
-            return Files.readString(path);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode node1 = objectMapper.readTree(text1);
+        JsonNode node2 = objectMapper.readTree(text2);
+
+        TreeSet<String> fields = new TreeSet<>();
+
+        Iterator<String> it1 = node1.fieldNames();
+        while (it1.hasNext()) {
+            fields.add(it1.next());
         }
-        public static String getDiff(String text1, String text2) throws JsonProcessingException {
+        Iterator<String> it2 = node2.fieldNames();
+        while (it2.hasNext()) {
+            fields.add(it2.next());
+        }
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode node1 = objectMapper.readTree(text1);
-            JsonNode node2 = objectMapper.readTree(text2);
-
-            TreeSet <String> fields = new TreeSet<>();
-
-            Iterator<String> it1 = node1.fieldNames();
-            while (it1.hasNext()) {
-                fields.add(it1.next());
-            }
-            Iterator<String> it2 = node2.fieldNames();
-            while (it2.hasNext()) {
-                fields.add(it2.next());
-            }
-
-            StringBuilder builder = new StringBuilder("{\n");
-            for (var field : fields) {
-                if (!node1.has(field)) {
-                    builder.append(String.format("  + %s: %s\n", field, node2.get(field).toString()));
-                } else if (!node2.has(field)) {
-                    builder.append(String.format("  - %s: %s\n", field, node1.get(field).toString()));
+        StringBuilder builder = new StringBuilder("{\n");
+        for (var field : fields) {
+            if (!node1.has(field)) {
+                builder.append(String.format("  + %s: %s\n", field, node2.get(field).toString()));
+            } else if (!node2.has(field)) {
+                builder.append(String.format("  - %s: %s\n", field, node1.get(field).toString()));
+            } else {
+                var value1 = node1.get(field).toString();
+                var value2 = node2.get(field).toString();
+                if (value1.equals(value2)) {
+                    builder.append(String.format("    %s: %s\n", field, node2.get(field).toString()));
                 } else {
-                    var value1 = node1.get(field).toString();
-                    var value2 = node2.get(field).toString();
-                    if (value1.equals(value2)){
-                        builder.append(String.format("    %s: %s\n", field, node2.get(field).toString()));
-                    } else {
-                        builder.append(String.format("  - %s: %s\n", field, node1.get(field).toString()));
-                        builder.append(String.format("  + %s: %s\n", field, node2.get(field).toString()));
-                    }
+                    builder.append(String.format("  - %s: %s\n", field, node1.get(field).toString()));
+                    builder.append(String.format("  + %s: %s\n", field, node2.get(field).toString()));
                 }
             }
-            builder.append("}");
-
-            return builder.toString().replace("\"", "");
         }
+        builder.append("}");
+
+        return builder.toString().replace("\"", "");
     }
+}
