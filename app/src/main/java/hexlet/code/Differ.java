c5fac6d2c7e384b12ccf0ac2fbf7c4cbd1758da6
maks <nickitkacozloff@yandex.ru>
2023-08-30 18:50:44 +0300

    modified Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -8,11 +8,31 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
+
 public class Differ {
+
+    private static final int SIGN = 0;
+    private static final int VALUE = 1;
+
     public static String differ(String text1, String text2) throws Exception {
-        return generate(text1, text2);
+        var diff = generate(text1, text2);
+
+        StringBuilder builder = new StringBuilder("{\n");
+        for (var field : diff.keySet()) {
+            var changes = diff.get(field);
+            for (var change : changes) {
+                var sign = change[SIGN];
+                var value = change[VALUE];
+                var line = String.format("  %s %s: %s\n", sign, field, value);
+                builder.append(line);
+            }
+        }
+        builder.append("}");
+        return builder.toString().replace("\"", "");
     }
 
     public static String parse(String readFilePath) throws Exception {
@@ -25,7 +45,8 @@ public class Differ {
         return Files.readString(path);
     }
 
-    public static String generate(String text1, String text2) throws JsonProcessingException {
+    public static TreeMap<String, LinkedList<String[]>> generate(String text1, String text2)
+            throws JsonProcessingException {
 
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode node1 = objectMapper.readTree(text1);
@@ -42,25 +63,26 @@ public class Differ {
             fields.add(it2.next());
         }
 
-        StringBuilder builder = new StringBuilder("{\n");
+        TreeMap<String, LinkedList<String[]>> result = new TreeMap<>();
         for (var field : fields) {
+            LinkedList<String[]> values = new LinkedList<>();
+            result.put(field, values);
             if (!node1.has(field)) {
-                builder.append(String.format("  + %s: %s\n", field, node2.get(field).toString()));
+                result.get(field).add(new String[]{"+", node2.get(field).toString()});
             } else if (!node2.has(field)) {
-                builder.append(String.format("  - %s: %s\n", field, node1.get(field).toString()));
+                result.get(field).add(new String[]{"-", node1.get(field).toString()});
             } else {
                 var value1 = node1.get(field).toString();
                 var value2 = node2.get(field).toString();
                 if (value1.equals(value2)) {
-                    builder.append(String.format("    %s: %s\n", field, node2.get(field).toString()));
+                    result.get(field).add(new String[]{" ", node2.get(field).toString()});
                 } else {
-                    builder.append(String.format("  - %s: %s\n", field, node1.get(field).toString()));
-                    builder.append(String.format("  + %s: %s\n", field, node2.get(field).toString()));
+                    result.get(field).add(new String[]{"-", node1.get(field).toString()});
+                    result.get(field).add(new String[]{"+", node2.get(field).toString()});
                 }
             }
         }
-        builder.append("}");
 
-        return builder.toString().replace("\"", "");
+        return result;
     }
 }
