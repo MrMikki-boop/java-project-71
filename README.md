e770605a6c2c9ef33bbbbdddcd9aae42886f952b
maks <nickitkacozloff@yandex.ru>
2023-09-17 17:41:38 +0300

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/MrMikki-boop/java-project-71/workflows/hexlet-check/badge.svg)](https://github.com/MrMikki-boop/java-project-71/actions)
+[![Java CI](https://github.com/MrMikki-boop/java-project-71/actions/workflows/main.yml/badge.svg)](https://github.com/MrMikki-boop/java-project-71/actions/workflows/main.yml)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/e405cb516ba1e2a051a4/maintainability)](https://codeclimate.com/github/MrMikki-boop/java-project-712/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/e405cb516ba1e2a051a4/test_coverage)](https://codeclimate.com/github/MrMikki-boop/java-project-712/test_coverage)
+# Differ (Education project)
+
+### Description
+It's a primitive application that compares two *flat* jsons and returns their difference.
+
+#### Example of Help Output (Project Step 4):
+[![asciicast](https://asciinema.org/a/608481.svg)](https://asciinema.org/a/608481)
+[![demo](https://asciinema.org/a/608481.svg)](https://asciinema.org/a/608481)
+
+#### Example of Comparing JSON Files (Project Step 5):
+[![asciicast](https://asciinema.org/a/608484.svg)](https://asciinema.org/a/608484)
+
+#### Example of Comparing YAML Files (Project Step 7):
+[![asciicast](https://asciinema.org/a/608485.svg)](https://asciinema.org/a/608485)
+
+#### Project Step 8: Output in Stylish Format:
+[![asciicast](https://asciinema.org/a/608486.svg)](https://asciinema.org/a/608486)
+
+#### Project Step 9: Output in Plain Format:
+[![asciicast](https://asciinema.org/a/608487.svg)](https://asciinema.org/a/608487)
+
+#### Project Step 10: Output in JSON Format:
+[![asciicast](https://asciinema.org/a/608488.svg)](https://asciinema.org/a/608488)
+
--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -7,38 +7,42 @@ public class Stylish {
     public static String makeStylish(List<Map<String, Object>> difference) {
 
         StringBuilder result = new StringBuilder();
-        result.append("{");
+        result.append("{").append(System.lineSeparator());
+        boolean isFirst = true;
+
         for (Map<String, Object> element : difference) {
-            result.append("\n").append("  ");
+            if (!isFirst) {
+                result.append(System.lineSeparator());
+            }
             if (element.get("status").equals("deleted")) {
-                result.append("- ")
+                result.append("  - ")
                         .append(element.get("key"))
                         .append(": ")
                         .append(element.get("oldValue"));
             } else if (element.get("status").equals("added")) {
-                result.append("+ ")
+                result.append("  + ")
                         .append(element.get("key"))
                         .append(": ")
                         .append(element.get("newValue"));
             } else if (element.get("status").equals("unchanged")) {
-                result.append("  ")
+                result.append("    ")
                         .append(element.get("key"))
                         .append(": ")
                         .append(element.get("oldValue"));
             } else if (element.get("status").equals("changed")) {
-                result.append("- ")
+                result.append("  - ")
                         .append(element.get("key"))
                         .append(": ")
-                        .append(element.get("oldValue"))
-                        .append("\n");
-                result.append("  ")
-                        .append("+ ")
+                        .append(element.get("oldValue"));
+                result.append(System.lineSeparator());
+                result.append("  + ")
                         .append(element.get("key"))
                         .append(": ")
                         .append(element.get("newValue"));
             }
+            isFirst = false;
         }
-        result.append("\n}");
-        return result.toString().trim();
+        result.append(System.lineSeparator()).append("}");
+        return result.toString();
     }
 }
