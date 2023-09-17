c259b82361e92816dc43fff4c3967c454d436052
maks <nickitkacozloff@yandex.ru>
2023-09-17 18:07:08 +0300

    refactor

--- app/src/main/java/hexlet/code/Tree.java
+++ app/src/main/java/hexlet/code/Tree.java
@@ -3,17 +3,16 @@ package hexlet.code;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.TreeMap;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.Objects;
 
 public class Tree {
     public static List<Map<String, Object>> makeDifference(Map<String, Object> mapA, Map<String, Object> mapB) {
         List<Map<String, Object>> list = new ArrayList<>();
 
-        Set<String> keys = new TreeSet<>();
-        keys.addAll(mapA.keySet());
+        Set<String> keys = new TreeSet<>(mapA.keySet());
         keys.addAll(mapB.keySet());
 
         for (String key : keys) {
@@ -21,23 +20,25 @@ public class Tree {
             var valueA = mapA.get(key);
             var valueB = mapB.get(key);
 
-            if (mapA.containsKey(key) && !mapB.containsKey(key)) {
-                diffMap.put("key", key);
-                diffMap.put("status", "deleted");
-                diffMap.put("oldValue", valueA);
-            } else if (!mapA.containsKey(key) && mapB.containsKey(key)) {
-                diffMap.put("key", key);
-                diffMap.put("status", "added");
-                diffMap.put("newValue", valueB);
-            } else if (mapA.containsKey(key) && mapB.containsKey(key) && Objects.equals(valueA, valueB)) {
-                diffMap.put("key", key);
+            diffMap.put("key", key);
+
+            if (Objects.equals(valueA, valueB)) {
                 diffMap.put("status", "unchanged");
                 diffMap.put("oldValue", valueA);
-            } else if (mapA.containsKey(key) && mapB.containsKey(key) && !Objects.equals(valueA, valueB)) {
-                diffMap.put("key", key);
-                diffMap.put("status", "changed");
-                diffMap.put("oldValue", valueA);
-                diffMap.put("newValue", valueB);
+            } else {
+                if (mapA.containsKey(key)) {
+                    diffMap.put("status", "deleted");
+                    diffMap.put("oldValue", valueA);
+                }
+                if (mapB.containsKey(key)) {
+                    diffMap.put("status", "added");
+                    diffMap.put("newValue", valueB);
+                }
+                if (mapA.containsKey(key) && mapB.containsKey(key)) {
+                    diffMap.put("status", "changed");
+                    diffMap.put("oldValue", valueA);
+                    diffMap.put("newValue", valueB);
+                }
             }
             list.add(diffMap);
         }
