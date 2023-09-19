6bf3cab26c7e30526348da9482389e49f0f72ea5
maks <nickitkacozloff@yandex.ru>
2023-09-19 23:07:04 +0300

    Refactor variable and method names in Differ class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,21 +7,21 @@ import java.util.List;
 import java.util.Map;
 
 public class Differ {
-    public static String generate(String filepath1, String filepath2, String format) throws Exception {
+    public static String generateAndFormat(String filepath1, String filepath2, String format) throws Exception {
         Path absPath1 = Paths.get(filepath1).toAbsolutePath().normalize();
         Path absPath2 = Paths.get(filepath2).toAbsolutePath().normalize();
         String content1 = Files.readString(absPath1);
         String content2 = Files.readString(absPath2);
 
-        Map<String, Object> mapA = Parser.makeParsing(filepath1, content1);
-        Map<String, Object> mapB = Parser.makeParsing(filepath2, content2);
+        Map<String, Object> parsedData1 = Parser.makeParsing(filepath1, content1);
+        Map<String, Object> parsedData2 = Parser.makeParsing(filepath2, content2);
 
-        List<Map<String, Object>> difference = Tree.makeDifference(mapA, mapB);
+        List<Map<String, Object>> difference = Tree.makeDifference(parsedData1, parsedData2);
         return Formatter.chooseFormat(difference, format);
     }
 
-    public static String generate(String filepath1, String filepath2) throws Exception {
+    public static String generateAndFormat(String filepath1, String filepath2) throws Exception {
         String format = "stylish";
-        return generate(filepath1, filepath2, format);
+        return generateAndFormat(filepath1, filepath2, format);
     }
 }
